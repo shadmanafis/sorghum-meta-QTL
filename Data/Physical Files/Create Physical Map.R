32bad6a23f4b54505d5ee8a453f1ee977b3cae78
ahmedaquib <17051002@rpcau.ac.in>
2021-07-06 13:32:52 +0530

    physical

--- Data/Physical Files/Create Physical Map.R
+++ Data/Physical Files/Create Physical Map.R
@@ -5,3 +5,4 @@ phy <- filter(phy, Chromosome==1) %>%
   slice(-grep("SB",phy$Locus.name)) %>%
   select(c("Locus.name","Physical.Map.Position"))
 phy[,1] <- gsub("X","",phy[,1])
+write.table(phy,"phy_r.txt",sep = "\t",quote = FALSE, row.names = TRUE)
