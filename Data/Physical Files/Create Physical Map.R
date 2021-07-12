2c58993a750cb98d64138557bb68bcd4abe55ebb
ahmedaquib <17051002@rpcau.ac.in>
2021-07-12 13:23:41 +0530

    chrom3done

--- Data/Physical Files/Create Physical Map.R
+++ Data/Physical Files/Create Physical Map.R
@@ -2,10 +2,12 @@ library(readr)
 library(tidyr)
 library(dplyr)
 phy <- read.table("Ramu et al.- Positions.txt", sep="\t", header = TRUE, row.names = 1) 
-phy <- filter(phy, Chromosome==3) %>%
-  slice(-grep("SB*",phy$Locus.name)) %>%
+phy <- filter(phy, Chromosome==3)
+#slice(-grep("SB",phy$Locus.name)) %>%
+  phy <- slice(phy,-grep("SB",phy$Locus.name)) %>%
   select(c("Locus.name","Physical.Map.Position"))
 phy[,1] <- gsub("X","",phy[,1])
+phy[,1] <- tolower(phy[,1])
 write.table(phy,"phy_r3.txt",sep = "\t",quote = FALSE, row.names = TRUE)
 
 #read genetic files
