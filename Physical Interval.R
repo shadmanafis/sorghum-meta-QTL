d6d957d1116405fbd3b7260acf664f1dddab33d6
ahmedaquib <17051002@rpcau.ac.in>
2021-07-19 20:10:13 +0530

    physint

--- Physical Interval.R
+++ Physical Interval.R
@@ -11,7 +11,7 @@ phy <- dplyr::slice(phy,-grep("SB",phy$`Locus name`)) %>%
   dplyr::select(`Locus name`,`Physical Map Position`)
 phy$`Locus name` <- gsub("X","",phy$`Locus name`) %>%
   tolower()
-
+phy <- phy[!duplicated(phy$`Locus name`),]
 # Set WOrking Directory, Granges list from all the consensus maps
 setwd("C:/Users/Shabana Usmani/sorghum-meta/sorghum-meta-QTL/Results/Consensus Maps")
 files <- list.files()
@@ -28,6 +28,23 @@ for(i in i){
 }
 remove(files,i,j,f, Chromosome)
 Consensus <- data.frame(chr=Consensus$Chromosome,start=Consensus$V3,end = Consensus$V3+1,feature=tolower(Consensus$V2))
+Consensus <- left_join(Consensus,phy, by=c("feature"="Locus name"))
+Consensus <- Consensus[is.na(Consensus$`Physical Map Position`)==FALSE,]
+Consensus <- Consensus[!duplicated(Consensus$feature),]
+
+Temp <- split.data.frame(Consensus, Consensus$chr)
+vec <- function(x){
+  v <- 0
+  for(i in 2:dim(x)[1]){
+    v <- c(v,x[i,5]-x[i-1,5])
+  }
+  return(v)
+}
+Temp <- sapply(Temp, vec, simplify = `array`)
+Temp <- unlist(Temp, use.names = FALSE)
+Consensus$Diff <- Temp
+Consensus <- Consensus[Consensus$Diff>=0,]
+
 GR <- makeGRangesFromDataFrame(Consensus, keep.extra.columns=TRUE)
 
 setwd("C:/Users/Shabana Usmani/sorghum-meta/sorghum-meta-QTL/Results")
@@ -37,14 +54,15 @@ gr <- makeGRangesFromDataFrame(meta)
 # Get markers inside or flanking the Meta-QTL
 df <- data.frame()
 for(i in 1:length(ranges(gr))){
-  hits <- subjectHits(findOverlaps(gr[i],GR))
-  if(length(hits)!=0){
-    df <- rbind(df,paste(GR[(min(hits)-1):(max(hits)+1)]$feature,collapse = ","))
-  } else {
+   hits <- subjectHits(findOverlaps(gr[i],GR))
+   if(length(hits)!=0){
+     df <- rbind(df,paste(GR[(min(hits)-1):(max(hits)+1)]$feature,collapse = ","))
+   } else {
       hits <- nearest(gr[i],GR)
      df <- rbind(df,paste(GR[(min(hits)-1):(max(hits)+1)]$feature,collapse = ","))
-  }
+   }
 }
+
 colnames(df) <- "markers"
 values(gr) <- df
 df <- data.frame(MQTL = row.names(df),markers = df$markers)%>%
@@ -53,4 +71,17 @@ df <- data.frame(MQTL = row.names(df),markers = df$markers)%>%
 
 df <- left_join(df,data.frame(MQTL=row.names(meta),meta[,3]),by=c("MQTL"="MQTL"))
 df <- right_join(df,Consensus, by=c("markers"="feature","chr"="chr"))[,1:5]
-write.table(df,"MQTL-Markers-Positions.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+df <- df[!is.na(df$MQTL),]
+df <- df[,c(1,4,2,5,3)]
+df <- df[!duplicated(df),]  
+colnames(df) <- c("MQTL","Chr","Marker","cM","Mb")
+
+getinterval <- function(i){
+  fit <- filter(df, df$MQTL==i)%>%
+        lm(formula=Mb~cM)%>%predict(newdata = data.frame(cM=unlist(meta[i,1:2], use.names = FALSE)), interval = "confidence", level=0.95)
+  fit <- c(fit[1,2],fit[dim(fit)[1],3])
+  return(fit)
+}
+P95 <- sapply(1:32,getinterval)
+P90 <- sapply(1:32,getinterval)
+P75 <- sapply(1:32,getinterval)
